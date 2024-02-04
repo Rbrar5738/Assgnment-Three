4ac9376a8fc2579d9c5e57cb58397d11a9a49c24
Rbrar5738 <rbrar5738@conestogac.on.ca>
2024-02-04 16:01:15 -0600

    Update display_profile.js
    Display data is the fields

--- Part2-profile/profile/display_profile.js
+++ Part2-profile/profile/display_profile.js
@@ -2,6 +2,11 @@
 
 $(document).ready( () => {
 	// display data from session storage
+
+	$('#email').html(sessionStorage.getItem("email"));	
+	$('#phone').html(sessionStorage.getItem("phone"));
+	$('#postal').html(sessionStorage.getItem("postal"));
+	$('#dob').html(sessionStorage.getItem("dob"));
 	
 	$("back").click( () => {
 		

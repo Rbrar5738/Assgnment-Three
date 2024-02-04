b0dd4fa9de81e6ebf7ee554facb776c6bf7b2c04
Rbrar5738 <rbrar5738@conestogac.on.ca>
2024-02-04 15:44:46 -0600

    Update login.js
    Cookies are set

--- Part1-login/login/login.js
+++ Part1-login/login/login.js
@@ -1,17 +1,37 @@
 "use strict";
 
 const getCookieByName = name => {
-    return "";
+    const cookies = document.cookie;
+
+    let begin = cookies.indexOf(name + "=");
+    if (begin === -1) {
+         return ""; 
+    }
+    else { 
+        begin = begin + (name.length + 1);
+        let end = cookies.indexOf(";", begin);
+        if (end === -1) { 
+            end = cookies.length;
+        }
+        const cookie_data = cookies.substring(begin, end);
+        return decodeURIComponent(cookie_data);
+    }
+
 };
 
 const setCookie = (name, value, days) => {
-
+    let cookie_info = name + "=" + encodeURIComponent(value);
+    if (days) {
+        cookie_info += "; max-age=" + days * 24 * 60 * 60;
+    }
+    cookie_info += "; path=/";
+    document.cookie = cookie_info;
 };
 
 const deleteCookie = name => {
-
+    document.cookie = name + "=''; max-age=0; path=/";
 };
 
 const goToPage = url => {
-
+    location.href = url;
 };
